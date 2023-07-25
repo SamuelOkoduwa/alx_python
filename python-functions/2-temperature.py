1f98b2490b283df720b77a1a3bd2435584964ad2
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-07-25 18:32:25 +0100

    Temperature Converter Function

---
+++ python-functions/2-temperature.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+def convert_to_celsius(fahrenheit): 
+    return ((5/9)*(fahrenheit - 32))
+# print(convert_to_celsius(100))
+# print(convert_to_celsius(-40))
+# print(convert_to_celsius(32))
