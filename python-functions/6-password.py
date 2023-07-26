13837dbae1f695087d942f79985a60f4454267d5
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-07-26 11:10:11 +0100

    Python function called validate_password that takes a password as input and performs some operations

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -4,19 +4,32 @@ def validate_password(password):
     valid = True
     if len(password) < 8:
         print('length should be at least 8')
+        valid = False       
+    if not any(char.isdigit() for char in password):
+        print("Password should have at least a digit")
         valid = False
-        
     if not any(char.isupper() for char in password):
         print("Password should have at least an uppercase")
         valid = False
     if not any(char.islower() for char in password):
         print("Password should have at least a lowercase")
         valid = False
-    if not any(char.isdigit() for char in password):
-        print("Password should have at least a digit")
+    if any(char.isspace() for char in password):
         valid = False
-    if not any(char.isspace() for char in password):
-        valid = False
-    # if valid:
     return valid
-# print(validate_password(input(" Please type your Password:")))
+
+# def validate_password(password):
+#     is_valid = True 
+#     if len(password) < 8: 
+#         is_valid = False    
+#     if not any(char.isdigit() for char in password): 
+#         is_valid = False
+          
+#     if not any(char.isupper() for char in password): 
+#         is_valid = False     
+#     if not any(char.islower() for char in password):  
+#         is_valid = False
+#     if any(char.isspace() for char in password): 
+#         is_valid = False
+#     return is_valid 
+print(validate_password(input(" Please type your Password:")))
