4ed0ee85d452f1ac545068f5039445811d820146
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-07-31 23:13:12 +0100

    the last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -9,8 +9,8 @@ else:
     last_digit = (-(abs(-number) % 10))
 print("Last digit of {} is ".format(number), end=" ")
 if last_digit > 5:
-    print("{} and is greater than 5" .format(last_digit), end='\n')
+    print("{:d} and is greater than 5" .format(last_digit), end='\n')
 if last_digit == 0:
-    print("{} and is 0" .format(last_digit), end='\n')
+    print("{:d} and is 0" .format(last_digit), end='\n')
 if last_digit < 6 and last_digit != 0:
-    print("{} and less than 6 and not 0" .format(last_digit), end='\n')
+    print("{:d} and less than 6 and not 0" .format(last_digit), end='\n')
