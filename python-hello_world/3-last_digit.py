bbee485696ba212507b89b2bb0a82d33f1c5b64e
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-07-31 23:28:49 +0100

    the last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -7,10 +7,10 @@ if number > 0:
     last_digit = number % 10
 else:
     last_digit = -((-number) % 10)
-print("Last digit of {} is ".format(number), end=" ")
+print("Last digit of {:d} is ".format(number), end="")
 if last_digit > 5:
     print("{:d} and is greater than 5" .format(last_digit), end='\n')
-if last_digit == 0:
+elif last_digit == 0:
     print("{:d} and is 0" .format(last_digit), end='\n')
-if last_digit < 6 and last_digit != 0:
+elif last_digit < 6 and last_digit != 0:
     print("{:d} and less than 6 and not 0" .format(last_digit), end='\n')
