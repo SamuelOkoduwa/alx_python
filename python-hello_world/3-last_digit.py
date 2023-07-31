42448eb0f4fc9ea917e739f8cbad746b65cc4684
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-07-31 23:35:20 +0100

    The last digit of a number updated

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,5 +1,4 @@
-
-#!/usr/bin/python3
+"""#!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
@@ -13,4 +12,21 @@ if last_digit > 5:
 elif last_digit == 0:
     print("{:d} and is 0" .format(last_digit), end='\n')
 elif last_digit < 6 and last_digit != 0:
-    print("{:d} and less than 6 and not 0" .format(last_digit), end='\n')
+    print("{:d} and less than 6 and not 0" .format(last_digit), end='\n')"""
+    
+
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+# YOUR CODE HERE
+if number > 0:
+    last_digit = number % 10
+else:
+    last_digit = -((-number) % 10)
+print("Last digit of {:d} is ".format(number), end='')
+if last_digit > 5:
+    print("{:d} and is greater than 5".format(last_digit))
+elif last_digit == 0:
+    print("{:d} and is 0".format(0))
+elif last_digit < 6 and last_digit != 0:
+    print("{:d} and is less than 6 and not 0".format(last_digit))
