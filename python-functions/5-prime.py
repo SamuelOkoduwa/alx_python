37698a9504967d8eb60b6eb8bfd734e531cbaf93
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-08-01 01:36:17 +0100

    Prime numbers

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 
+# def is_prime(number):
+#     if number % 2 == 1 and number % number == 0:
+#         return True
+#     else:
+#         return False
+        
+# print(is_prime(15))
+
 def is_prime(number):
-    if number % 2 == 0:
+    if number <= 1:
         return False
-    else:
-        return True
-    
-# print(is_prime(18))
+    for i in range(2,number):
+        if (number%i) == 0:
+            return False
+    return True
