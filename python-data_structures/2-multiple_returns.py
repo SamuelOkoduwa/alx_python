2e68d6220a4029e6511afd8046d65f13f4ad9e6b
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-08-04 10:54:53 +0100

    Write a function that returns with the length of a string and the first chararter

---
+++ python-data_structures/2-multiple_returns.py
@@ -0,0 +1,7 @@
+def multiple_returns(sentence):
+    length = len(sentence)
+    first = sentence[0]
+    if sentence == ():
+        first == None
+    else:
+        return ("Length: {:d} - First character: {}" .format(length, first))
