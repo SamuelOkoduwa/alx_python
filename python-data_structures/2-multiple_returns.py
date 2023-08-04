48dbfc38146826f2d28e3db1334795e79fa1bfcc
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-08-04 15:26:12 +0100

    Python function called validate_password that takes a password as input and performs some operations

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-'''
+
 def multiple_returns(sentence):
     length = len(sentence)
     first = sentence[0]
     if sentence == ():
         first == None
     else:
-        return ("Length: {:d} - First character: {}" .format(length, first))
-'''    
+        return (length, first)
+ 
 
-def multiple_returns(sentence):
-    return (len(sentence), sentence[0] if len(sentence) > 0 else None)
-print(multiple_returns("At Holberton school, I learnt C!"))
+# def multiple_returns(sentence):
+#     return (len(sentence), sentence[0] if len(sentence) > 0 else None)
+# print(multiple_returns("At Holberton school, I learnt C!"))
