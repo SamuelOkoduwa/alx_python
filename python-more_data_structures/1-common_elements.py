48621ea568f99d014626b42e37deb7317250681f
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-08-04 22:57:43 +0100

    A function that returns a set of common elemets in two sets

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -3,4 +3,4 @@
 def common_elements(set_1, set_2):
     c_set = set_1.intersection(set_2)
     return c_set
-print (common_elements(set_1=set_1, set_2=set_2))
+# print (common_elements(set_1=set_1, set_2=set_2))
