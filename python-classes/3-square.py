07819a91f89a7f31734b558617b88959a3fda58a
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-08-30 21:34:56 +0100

    a class Square based (task 2-square.py) with an instance attribute of size with property and property setter

---
+++ python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+# A class with an object Square
+class Square:
+    #A private instance
+    def __init__(self, size=0):
+        self.__size = size
+    #Property to retrieve instance of the class
+    @property
+    def size(self):
+        return self.__size
+    
+    #property setter of value argument
+    @property.setter
+    def size(self, value):
+        self.__value = value
+        if type(value) != int:
+            raise TypeError (" size must be an integer")
+        if value < 0:
+            raise ValueError ("size must be >= 0")
+    # A Public instance method of area
+    def area(self):
+        # the square of the current area
+        return self.__size * self.__size
