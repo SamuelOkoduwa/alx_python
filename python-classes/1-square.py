3816979f4e09fdd93977c6cfb0d7da2271ea9172
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-08-30 20:48:24 +0100

    a class square that defines a square by raising a TypeError and a Value Error

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Creating a class Square"""
+class Square:
+    """Instance of the class Square"""
+    def __init__(self, size=0):
+        self.__size = size
+        if size == int:
+            raise TypeError ("size must be an integer")
+        if size < 0:
+            raise ValueError ("size must be >= 0")
+        
+        
+        
+    
+        
+            
