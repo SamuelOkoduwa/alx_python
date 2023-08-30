d2ef9bb993667c8c079c4708019badb0e00bca59
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-08-30 15:42:59 +0100

    Write a Script that starts a web application modified

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -2,24 +2,15 @@
 
 from flask import Flask
 
-'''
-Initialization of the Flask Module
-
-'''
+'''Initialization of the Flask Module'''
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-"""
-Route
-
-"""
-@app.route("/")
+"""Route"""
+@app.route("/", strict_slashes= False)
 def Home():
     return "Hello HBNB!"
 
-""""
-running the flask application
-"""
+""""running the flask application"""
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host="0.0.0.0")
