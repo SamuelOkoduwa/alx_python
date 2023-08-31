ef4e49e430ef336104a49f50d84c7c2e05fee765
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-08-31 20:33:48 +0100

    a script that starts a flask web application that displays an html page if n is an integer

---
+++ python-web_framework/6-number_odd_or_even.py
@@ -0,0 +1,41 @@
+# !/usr/bin/python3
+
+"""Starting a web application using flask with a route displaying C followed by the value of the text variable
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def Hello_HBNB():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def HBNB():
+    return 'HBNB'
+
+@app.route('/c/<text>')
+def cisfun(text):
+    return 'C ' + text.replace('_', ' ')
+
+@app.route('/python/<text>')
+def pythoniscool(text='is cool'):
+    return 'Python ' + text.replace('_', ' ')
+
+@app.route('/number/<int:n>')
+def nisinteger(n):
+    return "{:d} is a number".format(n)
+
+@app.route('/number_template/<int:n>')
+def display_html(n):
+    """display a HTML page only if n is an integer"""
+    return render_template('5-number.html', n=n)
+
+@app.route("/number_odd_or_even/<n>")
+def even_or_odd(n):
+    return render_template("6-number_odd_even.html", n=n)
+        
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
