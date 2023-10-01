db066f11d3e414d2d3f40a8f6a95a9567d3f3abc
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-10-01 14:58:32 +0100

    assigning a random number to a variable and printing the last digit updated

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -6,7 +6,7 @@ number = random.randint(-10000, 10000)
 if number > 0:
     last_digit = number % 10
 else:
-    last_digit = (abs(number) % 10)
+    last_digit = (-(abs(-number) % 10))
 print("Last digit of {} is ".format(number), end="")
 if last_digit > 5:
     print("{} and is greater than 5" .format(last_digit))
@@ -14,18 +14,3 @@ if last_digit == 0:
     print("{} and is 0" .format(last_digit))
 if last_digit < 6 and last_digit != 0:
     print("{} and is less than 6 and not 0" .format(last_digit))
-    
-
-
-# # Extract the last digit of the number
-# last_digit = abs(number) % 10
-
-# # Print the result
-# print("The string Last digit of", number, "is", last_digit, end=" ")
-
-# if last_digit > 5:
-#     print("and is greater than 5")
-# elif last_digit == 0:
-#     print("and is 0")
-# else:
-#     print("and is less than 6 and not 0")
