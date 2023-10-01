0616d133874873edf7f6552c2db57b60f595efe3
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-10-01 17:39:15 +0100

    A function that returns a turple with the length of a string and it's first character updated

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 def multiple_returns(sentence): 
+    length = len(sentence)
+    first_charcater = sentence[0]
     if len(sentence) == 0:
         return "None"
-    else:
-        return(f"Length: {len(sentence)} - First character: {sentence[0]}")
-        # return(len(sentence), "- First character", sentence[0])
-# print(multiple_returns("At Holberton school, I learnt C!"))
+    else:      
+        return "Length: {:d} - First charater: {}".format(length, first_charcater)
+print(multiple_returns("At Holberton school, I learnt C!"))
+
+# def multiple_returns(sentence):
+#     # Check if the sentence is empty
+#     if len(sentence) == 0:
+#         return (0, None)
+#     else:
+#         return (len(sentence), sentence[0])
+
+# # Test the function
+# input_sentence = "Hello, world!"  # Change this to the sentence you want to test
+# result = multiple_returns(input_sentence)
+# print(f"Length: {result[0]}, First Character: {result[1]}")
