165e3510f9f6c780bf7ad7ec49a7d539e353b9c1
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-10-01 16:13:08 +0100

    A python function of fibonacci sequence that takes a number n as input and returns a list n

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 
+'''def fibonacci_sequence(n):
+    fib_sequence = []
+    a = 0
+    b = 1
+    for _ in (n):
+        fibonacci_sequence.append(a)
+        a = b
+        b = a + b
+        return fib_sequence
+    
+print(fibonacci_sequence(8))'''
+
 def fibonacci_sequence(n):
-    if n == 0:
-        return [0]
+    if n <= 0:
+        return []
     elif n == 1:
+        return [0]
+    elif n == 2:
         return [0, 1]
-    else:
-        Fst = fibonacci_sequence(n - 1)
-        Fst.append(Fst[-1] + Fst[-2])
-        return Fst
-# print(fibonacci_sequence(8))
+    
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < n:
+        next_num = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_num)
+    
+    return fib_sequence
+
+# Example usage:
+# n = 10
+# result = fibonacci_sequence(n)
+# print(result)  # Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 
