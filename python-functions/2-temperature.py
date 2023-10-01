cb3f49e8237ee8675fc7ebbaa0cce6745670073b
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-10-01 16:26:35 +0100

    A function that converts fahrenheit to celsius

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 def convert_to_celsius(fahrenheit): 
-    return ((5/9)* float(fahrenheit - 32))
-# print(convert_to_celsius(100))
+    return ((5/9)* (fahrenheit - 32))
+    # celsius = (fahrenheit - 32) * (5/9)
+    # return celsius
+print(convert_to_celsius(100))
 # print(convert_to_celsius(-40))
 # print(convert_to_celsius(32))
