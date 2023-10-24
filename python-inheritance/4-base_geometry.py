7423ed2762e4b28a94f12ba4df1b96d8d7945c92
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-10-24 16:53:39 +0100

    A class BaseGeometry with an instance of area

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+A class 'BaseGeometry'
+"""
+
+class BaseGeometry:
+    """A class of BaseGeometry."""
+    def area(self):
+        raise Exception("area() is not implemented")
+    
