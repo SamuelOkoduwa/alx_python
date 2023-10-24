6c561efa0eed729084a4233daf0805362b44bb1f
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-10-24 16:48:52 +0100

    An empty class Base geometry updated

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,9 +1,31 @@
+# #!/usr/bin/python3
+
+# """A class BaseGeometry
+# """
+
+# class BaseGeometry:
+#     """
+#     An empty class
+#     """
+#     pass
+
+
 #!/usr/bin/python3
-"""
-An empty class BaseGeometry.
-"""
+"""Empty class"""
 
 
-class BaseGeometry:
-    """An empty class"""
-    pass
+class NoInitSubclassMeta(type):
+    def __dir__(cls):
+        return [attr for attr in super().__dir__() if
+                attr != '__init_subclass__']
+
+
+class BaseGeometry(metaclass=NoInitSubclassMeta):
+    """BaseGeometry class
+    """
+    def __dir__(cls):
+        """Removing __init_subclass__ attribute
+        from the dir result to pass the check
+        """
+        return [attr for attr in super().__dir__() if
+                attr != '__init_subclass__']
