fff13b30bc8ef1d4d983b78963f7f4b2084f48bb
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-10-25 14:54:17 +0100

    A Python script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response.

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -4,15 +4,15 @@
 import requests
 import sys
 
-if __name__ == "__main":
-    url = sys.argv[1]  # Get the URL from the command-line argument
-    email = sys.argv[2]  # Get the email address from the command-line argument
 
-    # Create a dictionary with the email parameter
-    data = {'email': email}
+url = sys.argv[1]  # Get the URL from the command-line argument
+email = sys.argv[2]  # Get the email address from the command-line argument
 
-    # Send a POST request to the URL with the email parameter
-    response = requests.post(url, data=data)
+# Create a dictionary with the email parameter
+data = {'email': email}
 
-    # Display the body of the response
-    print(response.text)
+# Send a POST request to the URL with the email parameter
+response = requests.post(url, data=data)
+
+# Display the body of the response
+print(response.text)
